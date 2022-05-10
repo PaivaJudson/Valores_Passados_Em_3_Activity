bfe6458295ec62187af1484adecd95c22ed42be4
judsonpaiva <judson.paiva@hotmail.com>
2022-05-10 03:28:51 +0100

    Modifcando tela2 e Tela3.java

--- app/src/main/java/com/example/valores_passados_em_3_activity/Tela3.java
+++ app/src/main/java/com/example/valores_passados_em_3_activity/Tela3.java
@@ -2,13 +2,32 @@ package com.example.valores_passados_em_3_activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Tela3 extends AppCompatActivity {
+
+    TextView txtResultado;
+    Intent i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela3);
+
+        i = getIntent();
+        txtResultado = findViewById(R.id.txtResultado);
+        int valor1 = Integer.parseInt(i.getExtras().getString("valor1"));
+        int valor2 = Integer.parseInt(i.getExtras().getString("valor2"));
+
+        String result = valor1 + " + "+ valor2 +" = "+ (valor1 + valor2) + "\n"
+                      + valor1 + " - "+ valor2 +" = "+ (valor1 - valor2) + "\n"
+                      + valor1 + " * "+ valor2 +" = "+ (valor1 * valor2) + "\n"
+                      + valor1 + " / "+ valor2 +" = "+ (valor1 / valor2) + "\n";
+
+        txtResultado.setText(result);
+
     }
 }
